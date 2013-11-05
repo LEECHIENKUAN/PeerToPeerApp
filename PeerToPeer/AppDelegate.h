70a7fc44e795d98d2324166a9a40a461e3749997
Glenn Sayers <glennsayers.gs@gmail.com>
2013-11-05 21:53:26 +0000

    Initial Commit

---
+++ PeerToPeer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PeerToPeer
+//
+//  Created by Glenn Sayers on 05/11/2013.
+//  Copyright (c) 2013 Glenn Sayers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
